12c88d8b1a2b27c8ff8dc134d05733650e0cc930
Nishant <ni14@duke.edu>
2021-06-28 16:59:13 -0400

    prog

--- dbConfig.js
+++ dbConfig.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 const isProduction = process.env.NODE_ENV === "production";
+// const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+// const pool = new Pool({
+//   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+// });
+
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
 });
 
 module.exports = { pool };
